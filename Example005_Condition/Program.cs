fb807e646bf65d79f2c7e2e51de03e6d595c7e70
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-08 21:35:55 +0300

    Add Example005_Condition folder

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, it is Masha!");
+}
+else
+{
+    Console.WriteLine("Hey, ");
+    Console.WriteLine(username);
+}
